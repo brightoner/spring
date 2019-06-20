7009afd4a0015ab79d6f87a7587554503482ca43
brightoner <brightoner82@gmail.com>
2019-06-20 14:05:18 +0900

    spring ioc java config  @Configuation  @Bean     - 스프링 빈의 이름은 메소드 이름이 기본으로 적용된다

---
+++ src/test/java/kr/or/ddit/ioc/ApplicationIocConfig.java
@@ -0,0 +1,34 @@
+package kr.or.ddit.ioc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import kr.or.ddit.board.dao.BoardDao;
+import kr.or.ddit.board.dao.IboardDao;
+import kr.or.ddit.board.service.BoardService;
+
+@Configuration
+public class ApplicationIocConfig {
+
+	//	<bean id="boardDao" class="kr.or.ddit.board.dao.BoardDao"/>
+	@Bean
+	public IboardDao boardDao() {
+		return new BoardDao();
+	}
+	/*
+	<bean id="boardService" class="kr.or.ddit.board.service.BoardService">
+		<property name="boardDao" ref="boardDao"></property>
+	</bean>
+	*/
+	
+	@Bean
+	public BoardService boardService() {
+		BoardService boardService = new BoardService();
+		boardService.setName("brown");
+		boardService.setBoardDao(boardDao());
+		
+		return boardService;
+	}
+	
+	
+}
