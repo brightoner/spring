f2ac17b928ca716c90a181fefbcc05c8c3e662cc
brightoner <brightoner82@gmail.com>
2019-06-20 12:34:47 +0900

    spring ioc placeholder

---
+++ src/test/java/kr/or/ddit/ioc/SpringPlaceHolderTest.java
@@ -0,0 +1,38 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.ioc.placeholder.DbInfo;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-placeholder.xml")
+public class SpringPlaceHolderTest {
+
+	@Resource(name="dbInfo")
+	private DbInfo dbInfo;
+	
+	@Test
+	public void springPlaceHolderTest() {
+		/***Given***/
+		
+
+		/***When***/
+
+		/***Then***/
+		assertEquals("oracle.jdbc.driver.OracleDriver", dbInfo.getDriver());
+		assertEquals("jdbc:oracle:thin:@localhost:1521:xe", dbInfo.getUrl());
+		assertTrue(dbInfo.getUrl().contains("localhost:1521:xe"));
+		assertEquals("pc22", dbInfo.getUsername());
+		assertEquals("java", dbInfo.getPassword());
+		
+	}
+
+}
