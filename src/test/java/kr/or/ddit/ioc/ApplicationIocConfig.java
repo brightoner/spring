815df016c3cc7a81cc2ff26e4a02f422695e1533
brightoner <brightoner82@gmail.com>
2019-06-20 17:11:11 +0900

    spring 타입변환  - customDateEditor  - conversionService **두가지를 모두 설정할경우 customDateEditor가 실행

--- src/test/java/kr/or/ddit/ioc/ApplicationIocConfig.java
+++ src/test/java/kr/or/ddit/ioc/ApplicationIocConfig.java
@@ -7,20 +7,23 @@ import kr.or.ddit.board.dao.BoardDao;
 import kr.or.ddit.board.dao.IboardDao;
 import kr.or.ddit.board.service.BoardService;
 
-@Configuration
+@Configuration  //자바파일을 설정파일로
 public class ApplicationIocConfig {
 
 	//	<bean id="boardDao" class="kr.or.ddit.board.dao.BoardDao"/>
+	// ==> 변경
 	@Bean
 	public IboardDao boardDao() {
 		return new BoardDao();
 	}
+	
+	
 	/*
 	<bean id="boardService" class="kr.or.ddit.board.service.BoardService">
 		<property name="boardDao" ref="boardDao"></property>
 	</bean>
 	*/
-	
+	// ==> 변경
 	@Bean
 	public BoardService boardService() {
 		BoardService boardService = new BoardService();
---
+++ src/test/java/kr/or/ddit/ioc/ApplicationIocTypeConvertTest.java
@@ -0,0 +1,50 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.user.model.UserVo;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-type-convert.xml")
+public class ApplicationIocTypeConvertTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ApplicationIocTypeConvertTest.class);
+	
+	//userVo라는 이름의 스프링빈이 정상적으로 생성되었는지
+	@Resource(name="userVo")
+	private UserVo userVo;
+	
+
+	@Test
+	public void typeInjectionTest () {
+		/***Given***/
+		
+		/***When***/
+		Date birth = userVo.getBirth();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String birth_str = sdf.format(birth);
+		
+		
+		/***Then***/
+		assertNotNull(userVo);
+		assertEquals("2019-08-08",birth_str);
+		
+
+
+	}
+
+}
